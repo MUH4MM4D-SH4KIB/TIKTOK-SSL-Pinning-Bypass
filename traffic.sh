2fc111503ab50b6a460036bc6b48f327403e3171
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-02-14 10:37:35 +0600

    Add traffic.sh with HTTP request headers

---
+++ traffic.sh
@@ -0,0 +1,36 @@
+host: api22-normal-c-alisg.tiktokv.com
+content-length: 144
+cookie: store-idc=alisg
+cookie: tt-target-idc=alisg
+cookie: store-country-sign=MEIEDCWd_EEN6TUzm2b67AQg-8AS62kEjaBDXPoK--7Gi832kyNin4RQk3bG5Oq_MZkEEP7lnnmQbL2PPcnpLeaL6zQ
+cookie: store-country-code=bd
+cookie: store-country-code-src=did
+cookie: msToken=iNhR7Bd6qjLAPRBDHCmuWmNeD1yfgAnQL1JaIpd6WJ95yxDzmIDArhJBABaFw5gzxWqFGi4Vrrow8jvr74sWtUZhymtOAdJGaRt4zUq6MyQ=
+cookie: odin_tt=22d9c30d2dae50351e291e1f30e1f6070a1167cdd37009b1a4bfcbf4a06317853b43844e5d504c7f24e79c320128802f39ccbb5d1789d38f423c472a13304e35371f85642f9ba9843db20919e5df43af
+x-tt-pba-enable: 1
+x-tt-dm-status: login=0;ct=1;rt=6
+x-ss-req-ticket: 1771043385214
+sdk-version: 2
+passport-sdk-version: 1
+oec-cs-si-a: 2
+oec-cs-sdk-version: v10.02.01-ov-android_V31
+rpc-persist-pns-region-1: BD|1210997
+rpc-persist-pns-region-2: BD|1210997
+rpc-persist-pns-region-3: BD|1210997|1337179
+x-vc-bdturing-sdk-version: 2.4.0.i18n
+oec-vc-sdk-version: 3.2.1.i18n
+x-tt-request-tag: n=0;nr=0;bg=0;s=-1;p=0
+content-type: application/json; charset=UTF-8
+x-ss-stub: 92204B7AF4A944D4D4B0A2F129ADEBD0
+x-bd-content-encoding: gzip
+x-tt-store-region: bd
+x-tt-store-region-src: did
+rpc-persist-pyxis-policy-v-tnc: 1
+x-ss-dp: 1233
+x-tt-trace-id: 00-5a6971581062f376843e09c601a804d1-5a6971581062f376-01
+user-agent: com.zhiliaoapp.musically/2024309030 (Linux; U; Android 11; en; Z35; Build/RP1A.200720.011; Cronet/TTNetVersion:80a1e1d9 2026-01-21 QuicVersion:5f252c33 2025-12-30)
+accept-encoding: gzip, deflate, br
+x-argus: XsVNdJKwNxnFBlvvJhNe7LDs5RKS8smWG3x/IJjFkdrTvZCCm3FJ/aztGscnfv03tEMca38cws7ZN1vRQSLZEGbvl/wdgP8qA5YHcaCzk+2ZYOCvXjTda4Bs855WWuZmjM6E2KvsS4PNrUR0Fqha2ambH9pNGzPIoKDYWznqtcByUG8yWiT9Iigeg0532ChBcOl+8xt+TO1vgMZH8kSiCQQHpDdpNlQl2N1fTdMSjuvfdXio+tPcxnKn88i1xXlwxWLwEEB57qbi+GoL1hvkSeDlGocfAixs8XWEr0uPTpedDj9pEQy/WzKgR8zG+g+YZE9s9bKNa9p6Bq05w3Q4Oytw/l2TD2uNV6qsWjysWWMmBRnGNRrKPyoCBsE8QinGEAF7YTCItpFlD+JVPCVnqYtRWKVrT0R/psbDak/C/3SENNM3+WFchaBevhokqJ/a+wKlFuA0lC1giygwDC5QCm3oQp678xJ6dCHqCy9K86WZdY0G0KDvQ3d+kXvYtcMjnveRdDpvfhedh8dtH88L+s/+muklMatjNxRLk/nIc5aD/8+DzYmEuq/+CITFyqPxWSn5bXmrlU3I2XGCh5j6UfJD6/vfH80z3J2PplMjXt+M/4TfBuEUriohwJ8XckDeIJU=
+x-gorgon: 8404a0b80000b9027a34b9d07bf06479788c303c163037b3f257
+x-khronos: 1771043385
+x-ladon: eO67cGsv5e4NKNCa6AH/MifqPIkEhSNqgaMk79EqrE/itZlt
